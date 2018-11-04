b9965ccc2aea68982581f407c384e6a46686a723
Sana Rafaqat <sanarafaqat@Sanas-Air.lan>
2018-11-04 22:55:23 +0000

    V.1.2.0 Created Destroy in the Comments Controller

--- simpleblog/app/controllers/comments_controller.rb
+++ simpleblog/app/controllers/comments_controller.rb
@@ -4,8 +4,13 @@ class CommentsController < ApplicationController
      @comment = Post.comment.create (comment_params)
      redirect_to post_path(@post)
    end
-
-     private comment_params
+   def destroy
+     @post = Post.find(params[:post_id])
+     @comment = @post.comments.find(params[:id])
+     @comment.destroy
+     redirect_to post_path(@post)
+   end
+     private def comment_params
        params.require(:comment).permit(:username, :body)
      end
 end
